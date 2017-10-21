29c687305fb3f630aba7132b33d14034b3cf1d62
Daniel Collins <mail@dcollins.info>
2017-10-21 18:48:32 +1100

    add prefix to table names to guard against conflicts

--- worker.py
+++ worker.py
@@ -1,53 +1,53 @@
 import psycopg2
 
 sql = """
-CREATE TABLE scheduled (
+CREATE TABLE pgtq_scheduled (
   key INTEGER PRIMARY KEY,
   not_before TIMESTAMP WITHOUT TIME ZONE,
   task JSON,
   retried INTEGER
 );
 
-CREATE INDEX CONCURRENTLY ON scheduled (not_before);
+CREATE INDEX CONCURRENTLY ON pgtq_scheduled (not_before);
 
-CREATE TABLE runnable (
+CREATE TABLE pgtq_runnable (
   key INTEGER PRIMARY KEY,
   task JSON,
   retried INTEGER
 );
 
-CREATE TABLE running (
+CREATE TABLE pgtq_running (
   key INTEGER PRIMARY KEY,
   task JSON,
   retried INTEGER
 );
 
-CREATE TABLE complete (
+CREATE TABLE pgtq_complete (
   key INTEGER PRIMARY KEY,
   task JSON,
   retried INTEGER
 );
 
-PREPARE lock_task AS
+PREPARE pgtq_lock_task AS
   WITH task
-  AS (DELETE FROM runnable a
+  AS (DELETE FROM pgtq_runnable a
       WHERE a.ctid = (SELECT ctid
-                      FROM runnable
+                      FROM pgtq_runnable
                       ORDER BY key
                       FOR UPDATE SKIP LOCKED
                       LIMIT 1)
       RETURNING *)
-    INSERT INTO running SELECT * FROM task RETURNING *;
+    INSERT INTO pgtq_running SELECT * FROM task RETURNING *;
 
-PREPARE mark_completed (INTEGER) AS
+PREPARE pgtq_mark_completed (INTEGER) AS
   WITH task
-  AS (DELETE FROM running WHERE key=$1 RETURNING *)
-  INSERT INTO complete SELECT * FROM task RETURNING *;
+  AS (DELETE FROM pgtq_running WHERE key=$1 RETURNING *)
+  INSERT INTO pgtq_complete SELECT * FROM task RETURNING *;
 
-PREPARE mark_interupted(INTEGER) AS
+PREPARE pgtq_mark_interupted(INTEGER) AS
   WITH task
-  AS (DELETE FROM running WHERE key=$1 RETURNING *)
-  INSERT INTO scheduled
+  AS (DELETE FROM pgtq_running WHERE key=$1 RETURNING *)
+  INSERT INTO pgtq_scheduled
     SELECT key,
            (now() at time zone 'utc') +
             (INTERVAL '1 second' * random() * retried),
@@ -55,15 +55,15 @@ PREPARE mark_interupted(INTEGER) AS
            (retried + 1)
     FROM task RETURNING *;
 
-PREPARE run_scheduled AS
+PREPARE pgtq_run_scheduled AS
   WITH ready AS
-    (DELETE FROM scheduled a
+    (DELETE FROM pqtq_scheduled a
      WHERE
        a.ctid IN (SELECT ctid
-                  FROM scheduled
+                  FROM pgtq_scheduled
                   WHERE not_before < (now() at time zone 'utc'))
      RETURNING *)
-  INSERT INTO runnable(key) SELECT key FROM ready RETURNING *;
+  INSERT INTO pgtq_runnable(key) SELECT key FROM ready RETURNING *;
 """
 
 class PgTq(object):
