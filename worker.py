015f88f770b0bb91b4a187cd31c7c1c3a628befc
Daniel Collins <mail@dcollins.info>
2017-10-21 18:56:48 +1100

    prefix table names with queue name to allow one db to support multiple queues

--- worker.py
+++ worker.py
@@ -1,7 +1,7 @@
 import psycopg2
 
-sql = """
-CREATE TABLE IF NOT EXISTS pgtq_scheduled (
+sql_template = """
+CREATE TABLE IF NOT EXISTS pgtq_{1}_scheduled (
   key INTEGER PRIMARY KEY,
   not_before TIMESTAMP WITHOUT TIME ZONE,
   task JSON,
@@ -9,46 +9,46 @@ CREATE TABLE IF NOT EXISTS pgtq_scheduled (
 );
 
 CREATE INDEX CONCURRENTLY IF NOT EXISTS
-  ix_pgtq_scheduled_not_before ON pgtq_scheduled (not_before);
+  ix_pgtq_{1}_scheduled_not_before ON pgtq_{1}_scheduled (not_before);
 
-CREATE TABLE IF NOT EXISTS pgtq_runnable (
+CREATE TABLE IF NOT EXISTS pgtq_{1}_runnable (
   key INTEGER PRIMARY KEY,
   task JSON,
   retried INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS pgtq_running (
+CREATE TABLE IF NOT EXISTS pgtq_{1}_running (
   key INTEGER PRIMARY KEY,
   task JSON,
   retried INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS pgtq_complete (
+CREATE TABLE IF NOT EXISTS pgtq_{1}_complete (
   key INTEGER PRIMARY KEY,
   task JSON,
   retried INTEGER
 );
 
-PREPARE pgtq_lock_task AS
+PREPARE pgtq_{1}_lock_task AS
   WITH task
-  AS (DELETE FROM pgtq_runnable a
+  AS (DELETE FROM pgtq_{1}_runnable a
       WHERE a.ctid = (SELECT ctid
-                      FROM pgtq_runnable
+                      FROM pgtq_{1}_runnable
                       ORDER BY key
                       FOR UPDATE SKIP LOCKED
                       LIMIT 1)
       RETURNING *)
-    INSERT INTO pgtq_running SELECT * FROM task RETURNING *;
+    INSERT INTO pgtq_{1}_running SELECT * FROM task RETURNING *;
 
-PREPARE pgtq_mark_completed (INTEGER) AS
+PREPARE pgtq_{1}_mark_completed (INTEGER) AS
   WITH task
-  AS (DELETE FROM pgtq_running WHERE key=$1 RETURNING *)
-  INSERT INTO pgtq_complete SELECT * FROM task RETURNING *;
+  AS (DELETE FROM pgtq_{1}_running WHERE key=$1 RETURNING *)
+  INSERT INTO pgtq_{1}_complete SELECT * FROM task RETURNING *;
 
-PREPARE pgtq_mark_interupted(INTEGER) AS
+PREPARE pgtq_{1}_mark_interupted(INTEGER) AS
   WITH task
-  AS (DELETE FROM pgtq_running WHERE key=$1 RETURNING *)
-  INSERT INTO pgtq_scheduled
+  AS (DELETE FROM pgtq_{1}_running WHERE key=$1 RETURNING *)
+  INSERT INTO pgtq_{1}_scheduled
     SELECT key,
            (now() at time zone 'utc') +
             (INTERVAL '1 second' * random() * retried),
@@ -56,15 +56,15 @@ PREPARE pgtq_mark_interupted(INTEGER) AS
            (retried + 1)
     FROM task RETURNING *;
 
-PREPARE pgtq_run_scheduled AS
+PREPARE pgtq_{1}_run_scheduled AS
   WITH ready AS
-    (DELETE FROM pqtq_scheduled a
+    (DELETE FROM pqtq_{1}_scheduled a
      WHERE
        a.ctid IN (SELECT ctid
-                  FROM pgtq_scheduled
+                  FROM pgtq_{1}_scheduled
                   WHERE not_before < (now() at time zone 'utc'))
      RETURNING *)
-  INSERT INTO pgtq_runnable(key) SELECT key FROM ready RETURNING *;
+  INSERT INTO pgtq_{1}_runnable(key) SELECT key FROM ready RETURNING *;
 """
 
 class PgTq(object):
@@ -74,6 +74,7 @@ class PgTq(object):
         self.conn = psycopg2.connect(connection_string)
 
     def create_tables(self):
+        sql = sql_template.format(self.name)
         with self.conn:
             with conn.cursor() as cursor:
                 cur.execute(sql)
